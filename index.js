60e3703677b38ca9d0fca05c336c38ef2bb042bc
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-15 18:12:29 -0400

    version2 added

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-$(document).ready(function() {
-  //this is a function to call on submitting the search field
-  $("#cityInputBut").click(function() {
-    //load github users
-    var search = $("#weatherCityInput").val();
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", `api.openweathermap.org/data/2.5/weather?q=${search}&APPID={addkey}`, true);
-    xhr.onload = function() {
-      console.log(xhr);
-    };
-    xhr.send();
-  });
-});
+function update(weather) {
+  wind.innerHTML = weather.wind;
+  direction.innerHTML = weather.direction;
+  humidity.innerHTML = weather.humidity;
+  loc.innerHTML = weather.loc;
+  temp.innerHTML = weather.temp;
+  icon.src = "imgs/codes/" + weather.icon + ".png";
+};
+
+window.onload = function() {
+  temp = document.getElementById("temperature");
+  loc = document.getElementById("location");
+  icon = document.getElementById("icon");
+  humidity = document.getElementById("humidity");
+  wind = document.getElementById("wind");
+  direction = document.getElementById("direction");
+
+  var weather = {};
+  weather.wind = 3.5;
+  weather.humidity = 35;
+  weather.direction = "N";
+  weather.loc = "Providence";
+  weather.temp = "50";
+  weather.icon = 300;
+
+  update(weather);
+}
